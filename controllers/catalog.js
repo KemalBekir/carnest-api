98d9e5efc119d2b223d1a732a600e7faff74564d
Kemal <outsider.pld@gmail.com>
2023-12-11 23:33:04 +0000

    Created - User model, create endpoint Modified - car model

--- controllers/catalog.js
+++ controllers/catalog.js
@@ -3,6 +3,7 @@ const mapErrors = require("../utils/mappers");
 const { isAuth, isOwner } = require("../middleware/guards");
 const api = require("../services/car");
 const preload = require("../middleware/preload");
+const carFeatures = require("../types/carFeatures");
 
 router.get("/", async (req, res) => {
   const page = parseInt(req.query.page) || 1;
@@ -10,6 +11,42 @@ router.get("/", async (req, res) => {
 
   const data = await api.getAll(page, limit);
   res.json(data);
+});
+
+router.get("/features", async (req, res) => {
+  const data = carFeatures;
+  res.json(data);
+});
+
+/*TODO: add middleware for Authenticated and Role */
+router.post("/", async (req, res) => {
+  const car = {
+    make: req.body.make,
+    model: req.body.model,
+    images: req.body.images,
+    price: req.body.price,
+    age: req.body.age,
+    mileage: req.body.mileage,
+    fuelType: req.body.fuelType,
+    location: req.body.location,
+    colour: req.body.colour,
+    bodyType: req.body.bodyType,
+    gearbox: req.body.gearbox,
+    engineSize: req.body.engineSize,
+    specification: req.body.specification, // Array of strings for featurereq.body.s
+    doors: req.body.doors,
+    seats: req.body.seats,
+    reserved: req.body.reserved,
+  };
+
+  try {
+    const result = await api.create(car);
+    res.status(201).json(result);
+  } catch (err) {
+    const error = mapErrors(err);
+    console.error(err.message);
+    res.status(400).json({ message: error });
+  }
 });
 
 router.get("/:id", preload(), (req, res) => {
--- models/carModel.js
+++ models/carModel.js
@@ -5,44 +5,49 @@ const {
 } = require("mongoose");
 
 const CarModels = require("../types/carTypes");
+const { boolean } = require("webidl-conversions");
 
 const CarMakeEnumValues = Object.keys(CarModels).reduce((acc, make) => {
   acc[make] = make;
   return acc;
 }, {});
 
-const CarSchema = new Schema({
-  make: {
-    type: String,
-    required: true,
-    enum: Object.values(CarMakeEnumValues),
-  },
-  model: {
-    type: String,
-    required: true,
-    validate: {
-      validator: function (value) {
-        const make = this.get("make");
-        return validateModel(value, make);
-      },
-      message: "Invalid model for the selected make.",
+const CarSchema = new Schema(
+  {
+    make: {
+      type: String,
+      required: true,
+      enum: Object.values(CarMakeEnumValues),
     },
+    model: {
+      type: String,
+      required: true,
+      validate: {
+        validator: function (value) {
+          const make = this.get("make");
+          return validateModel(value, make);
+        },
+        message: "Invalid model for the selected make.",
+      },
+    },
+    images: [{ type: ObjectId, ref: "Image" }],
+    price: { type: Number, required: true },
+    age: { type: Number, required: true },
+    mileage: { type: Number, required: true },
+    fuelType: { type: String, required: true },
+    location: { type: String, required: true },
+    colour: { type: String, required: true },
+    bodyType: { type: String, required: true },
+    gearbox: { type: String, required: true },
+    engineSize: { type: String, required: true },
+    specification: [{ type: ObjectId, ref: "Specification" }], // Array of strings for features
+    doors: { type: Number, required: true },
+    seats: { type: Number, required: true },
+    reserved: { type: Boolean, default: false },
+    owner: { type: ObjectId, ref: "User" },
   },
-  images: [{ type: ObjectId, ref: "Image" }],
-  price: { type: Number, required: true },
-  age: { type: Number, required: true },
-  mileage: { type: Number, required: true },
-  fuelType: { type: String, required: true },
-  location: { type: String, required: true },
-  colour: { type: String, required: true },
-  bodyType: { type: String, required: true },
-  gearbox: { type: String, required: true },
-  engineSize: { type: String, required: true },
-  specification: [{ type: ObjectId, ref: "Specification" }], // Array of strings for features
-  doors: { type: Number, required: true },
-  seats: { type: Number, required: true },
-  //TODO add owner
-});
+  { timestamps: true }
+);
 
 const Car = model("Car", CarSchema);
 
